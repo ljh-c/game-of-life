39ea6440e51219c39bf55845e1a6132f8857e1d0
ljh-c <57099181+ljh-c@users.noreply.github.com>
2020-05-27 03:05:32 -0700

    [feat] Add double buffering and functionality to step through generations

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,6 +1,7 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef, useCallback, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Container } from '@material-ui/core';
+import NavigateNextOutlinedIcon from '@material-ui/icons/NavigateNextOutlined';
 
 const useStyles = makeStyles((theme) => ({
   board: {
@@ -13,55 +14,74 @@ const Board = () => {
 
   const CELL_SIZE = 20;
   const ROWS = 25;
-  const COLS = 25;
+  const COLS = 30;
 
-  const [matrix] = useState(
-    new Array(ROWS)
+  const [matrix, setMatrix] = useState(
+    new Array(COLS)
       .fill(0)
       .map(() =>
-        new Array(COLS).fill(0).map(() => Math.floor(Math.random() * 2))
+        new Array(ROWS).fill(0).map(() => Math.floor(Math.random() * 2))
       )
   );
 
   // Empty matrix
-  // new Array(ROWS).fill(new Array(COLS).fill(0))
+  // new Array(COLS).fill(new Array(ROWS).fill(0))
 
   // Random matrix
-  // new Array(ROWS)
+  // new Array(COLS)
   //   .fill(0)
   //   .map(() =>
-  //     new Array(COLS).fill(0).map(() => Math.floor(Math.random() * 2))
+  //     new Array(ROWS).fill(0).map(() => Math.floor(Math.random() * 2))
   //   )
+
+  const calcNext = (grid) => {
+    const nextMatrix = grid.map((col) => col.map((cell) => (cell ? 0 : 1)));
+
+    return nextMatrix;
+  };
+
+  const [buffer, setBuffer] = useState(calcNext(matrix));
 
   const canvasRef = useRef(null);
 
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext('2d');
-
-    // Draw board
-    matrix.forEach((row, y) => {
-      row.forEach((cell, x) => {
+  const drawBoard = useCallback((ctx) => {
+    matrix.forEach((col, x) => {
+      col.forEach((cell, y) => {
         ctx.beginPath();
         ctx.rect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
         ctx.strokeStyle = 'darkcyan';
         ctx.fillStyle = 'deepskyblue';
 
         if (cell) ctx.fill();
-        
+
         ctx.stroke();
       });
     });
   }, [matrix]);
 
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
+
+    ctx.clearRect(0, 0, CELL_SIZE * COLS, CELL_SIZE * ROWS)
+    drawBoard(ctx);
+  }, [drawBoard]);
+
+  const advanceGen = () => {
+    setMatrix(buffer);
+    setBuffer(calcNext(buffer));
+  };
+
   return (
     <div className={classes.board}>
       <Container maxWidth="md">
+        <NavigateNextOutlinedIcon onClick={advanceGen} />
+        <br />
         <canvas
           ref={canvasRef}
           width={CELL_SIZE * COLS}
           height={CELL_SIZE * ROWS}
-          style={{border: "1px solid darkcyan"}}
+          style={{ border: '1px solid darkcyan' }}
         />
       </Container>
     </div>
